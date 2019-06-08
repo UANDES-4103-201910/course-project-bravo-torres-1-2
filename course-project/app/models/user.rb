82185312e88cfd09a307c5df2dddf46ddf0d6399
benjabc <bdbravo@miuandes.cl>
2019-06-07 23:24:36 -0400

    added avatar to users, and users can change their avatars

--- course-project/app/models/user.rb
+++ course-project/app/models/user.rb
@@ -11,6 +11,7 @@ class User < ApplicationRecord
   has_many :posts
   has_many :reports
   has_many :comments, dependent: :destroy
+  has_one_attached :avatar
   def self.sign_in_from_omniauth(auth)
     find_by(provider: auth['provider'], uid: auth['uid'] ) || create_user_from_omniauth(auth)
   end
